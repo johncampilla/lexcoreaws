40afd4cb8726dab1af9d71945b5bf7fc5de5b275
johncampilla <jclextrim@gmail.com>
2024-03-06 07:05:39 +0800

    Update setting.py

--- lexcore/settings.py
+++ lexcore/settings.py
@@ -107,30 +107,30 @@ WSGI_APPLICATION = 'lexcore.wsgi.application'
 # }
 
 #==============My Postgresql in local ==================
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'lexcoredb',
-        'USER': 'postgres',
-        'PASSWORD':'640515john',
-        'HOST':'localhost',
-        'PORT':'5432',
-    }
- }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'lexcoredb',
+#         'USER': 'postgres',
+#         'PASSWORD':'640515john',
+#         'HOST':'localhost',
+#         'PORT':'5432',
+#     }
+#  }
 
 #========================================================
 
 #==================For Cloud Database======================================
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'lexcoreDB',
-#         'USER': 'lexlinkuser',
-#         'PASSWORD': 'lexlinkuserjohn',
-#         'HOST': 'lexcoredb.cfms4k8s6k1z.ap-southeast-1.rds.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lexcoreDB',
+        'USER': 'lexlinkuser',
+        'PASSWORD': 'lexlinkuserjohn',
+        'HOST': 'lexcoredb.cfms4k8s6k1z.ap-southeast-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 #============================================================================
 
 
@@ -187,7 +187,21 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+#==================For AWS Cloud Database======================================
 
+AWS_ACCESS_KEY_ID = 'AKIAUW6XQROOL5YCJVOY'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'lexcorebucket'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'ap-southeast-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.Emailbackend'
+
+#==================For Cloud Database======================================
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
